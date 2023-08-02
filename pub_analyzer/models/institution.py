c790075c7d898fb07c24ecfe640f1a4111399014
Alejandro Gaspar <miguel.gaspar03@hotmail.com>
2023-08-01 23:55:59 -0600

    Add Institution model

--- pub_analyzer/models/institution.py
+++ pub_analyzer/models/institution.py
@@ -1,6 +1,8 @@
-"""Institutions models."""
+"""Institutions models from OpenAlex API Schema definition."""
 
-from pydantic import BaseModel
+from enum import Enum
+
+from pydantic import BaseModel, Field, HttpUrl
 
 
 class DehydratedInstitution(BaseModel):
@@ -11,3 +13,88 @@ class DehydratedInstitution(BaseModel):
     display_name: str
     country_code: str
     type: str
+
+
+class InstitutionIDs(BaseModel):
+    """IDs from a Institution."""
+
+    openalex: HttpUrl
+    grid: str | None = None
+    ror: HttpUrl | None = None
+    wikipedia: HttpUrl | None = None
+    wikidata: HttpUrl | None = None
+
+
+class InstitutionType(str, Enum):
+    """Institution type."""
+
+    Education = "Education"
+    Healthcare = "Healthcare"
+    Company = "Company"
+    Archive = "Archive"
+    Nonprofit = "Nonprofit"
+    Government = "Government"
+    Facility = "Facility"
+    Other = "Other"
+
+
+class InstitutionSummaryStats(BaseModel):
+    """Citation metrics for this Institution."""
+
+    two_yr_mean_citedness: float = Field(..., alias="2yr_mean_citedness")
+    h_index: int
+    i10_index: int
+
+
+class InstitutionYearCount(BaseModel):
+    """Summary of published papers and number of citations in a year."""
+
+    year: int
+    works_count: int
+    cited_by_count: int
+
+
+class InstitutionGeo(BaseModel):
+    """Location of the institution."""
+
+    city: str
+    geonames_city_id: str
+    region: str | None = None
+    country_code: str
+    country: str
+
+
+class InstitutionRoleType(str, Enum):
+    """Posible institution roles."""
+
+    funder = "Funder"
+    publisher = "Publisher"
+    institution = "Institution"
+
+
+class InstitutionRole(BaseModel):
+    """Institution role."""
+
+    role: InstitutionRoleType
+    id: HttpUrl
+    works_count: int
+
+
+class Institution(BaseModel):
+    """Universities and other organizations to which authors claim affiliations."""
+
+    id: HttpUrl
+    ids: InstitutionIDs
+
+    display_name: str
+    country_code: str
+    type: InstitutionType
+    homepage_url: HttpUrl | None = None
+
+    works_count: int
+    cited_by_count: int
+    counts_by_year: list[InstitutionYearCount]
+    summary_stats: InstitutionSummaryStats
+
+    geo: InstitutionGeo
+    roles: list[InstitutionRole]
