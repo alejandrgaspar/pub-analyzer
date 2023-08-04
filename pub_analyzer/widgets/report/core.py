b233b1aa0bf59dd443dc83cb85b89a5124305b49
Alejandro Gaspar <miguel.gaspar03@hotmail.com>
2023-08-04 15:00:23 -0600

    Add support to load institution reports

--- pub_analyzer/widgets/report/core.py
+++ pub_analyzer/widgets/report/core.py
@@ -1,8 +1,9 @@
 """Main Report widgets."""
 
 import pathlib
+from enum import Enum
 
-from pydantic import TypeAdapter
+from pydantic import TypeAdapter, ValidationError
 from textual import on
 from textual.app import ComposeResult
 from textual.containers import Container, Horizontal
@@ -13,7 +14,7 @@ from pub_analyzer.internal.report import make_author_report, make_institution_re
 from pub_analyzer.models.author import Author
 from pub_analyzer.models.institution import Institution
 from pub_analyzer.models.report import AuthorReport, InstitutionReport
-from pub_analyzer.widgets.common import FileSystemSelector
+from pub_analyzer.widgets.common import FileSystemSelector, Select
 
 from .author import AuthorReportPane
 from .export import ExportReportPane
@@ -130,6 +131,19 @@ class CreateInstitutionReportWidget(CreateReportWidget):
 class LoadReportWidget(Static):
     """Widget report wrapper to load data from disk."""
 
+    class EntityType(Enum):
+        """Entity reports type."""
+
+        AUTHOR = AuthorReport
+        INSTITUTION = InstitutionReport
+
+    class EntityTypeSelector(Select[EntityType]):
+        """Entity type Selector."""
+
+    def __init__(self, entity_handler: EntityType = EntityType.AUTHOR) -> None:
+        self.entity_handler = entity_handler
+        super().__init__()
+
     @on(FileSystemSelector.FileSelected)
     def enable_button(self, event: FileSystemSelector.FileSelected) -> None:
         """Enable button on file select."""
@@ -149,18 +163,50 @@ class LoadReportWidget(Static):
 
         with open(file_path) as file:
             data = file.read()
-            report: AuthorReport = TypeAdapter(AuthorReport).validate_json(data)
-        report_widget = AuthorReportWidget(report=report)
 
         main_content = self.app.query_one(MainContent)
-        await main_content.query("*").exclude("#page-title").remove()
-        await main_content.mount(report_widget)
+        try:
+            match self.entity_handler:
+                case self.EntityType.AUTHOR:
+                    author_report: AuthorReport = TypeAdapter(AuthorReport).validate_json(data)
+                    author_report_widget = AuthorReportWidget(report=author_report)
+                    await main_content.query("*").exclude("#page-title").remove()
 
-        main_content.update_title(title=report.author.display_name)
+                    await main_content.mount(author_report_widget)
+                    main_content.update_title(title=author_report.author.display_name)
+
+                case self.EntityType.INSTITUTION:
+                    institution_report: InstitutionReport = TypeAdapter(InstitutionReport).validate_json(data)
+                    institution_report_widget = InstitutionReportWidget(report=institution_report)
+                    await main_content.query("*").exclude("#page-title").remove()
+
+                    await main_content.mount(institution_report_widget)
+                    main_content.update_title(title=institution_report.institution.display_name)
+        except ValidationError:
+            self.app.notify(
+                    title="Error loading report!",
+                    message="The report does not have the correct structure. This may be because it is an old version or because it is not of the specified type.",  # noqa: E501
+                    severity="error",
+                    timeout=10.0
+                )
+
+    @on(Select.Changed)
+    async def on_select_entity(self, event: Select.Changed) -> None:
+        """Change entity handler."""
+        match event.value:
+            case self.EntityType.AUTHOR:
+                self.entity_handler = self.EntityType.AUTHOR
+
+            case self.EntityType.INSTITUTION:
+                self.entity_handler = self.EntityType.INSTITUTION
 
     def compose(self) -> ComposeResult:
         """Compose load report widget."""
-        yield FileSystemSelector(path=pathlib.Path.home(), only_dir=False, extension=[".json",])
+        with Horizontal(classes="filesystem-selector-container"):
+            entity_options = [(name.title(), endpoint) for name, endpoint in self.EntityType.__members__.items()]
+
+            yield FileSystemSelector(path=pathlib.Path.home(), only_dir=False, extension=[".json",])
+            yield self.EntityTypeSelector(options=entity_options, value=self.entity_handler, allow_blank=False)
 
         with Horizontal(classes="button-container"):
             yield Button("Load Report", variant="primary", disabled=True, id="load-report-button")
