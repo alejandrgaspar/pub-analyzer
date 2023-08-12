6c7b96e990edccc5e9706dccd8c97eb3196edbde
Alejandro Gaspar <miguel.gaspar03@hotmail.com>
2023-08-11 18:05:26 -0600

    Migrate Cited by table to DataTable

--- pub_analyzer/widgets/report/work.py
+++ pub_analyzer/widgets/report/work.py
@@ -3,7 +3,6 @@
 import math
 from urllib.parse import quote
 
-from rich.table import Table
 from rich.text import Text
 from textual import events, on
 from textual.app import ComposeResult
@@ -29,9 +28,8 @@ class CitedByTable(Static):
     """Table with the summary of the works that cite a work."""
 
     DEFAULT_CSS = """
-    CitedByTable .citations-table {
+    CitedByTable DataTable #cited-by-table {
         height: auto;
-        margin: 1 0 0 0;
     }
     """
 
@@ -39,21 +37,34 @@ class CitedByTable(Static):
         self.citations_list = citations_list
         super().__init__()
 
+    @on(DataTable.RowHighlighted)
+    async def focus_row(self, message: DataTable.RowHighlighted) -> None:
+        """Focus row."""
+        citations_table: DataTable[Text] = self.app.query_one("#cited-by-table", DataTable)
+        citations_table.get_row(message.row_key)
+
     def compose(self) -> ComposeResult:
-        """Compose Table."""
-        citations_table = Table(title='Cited By', expand=True, show_lines=True)
+        """Compose table."""
+        yield DataTable(zebra_stripes=True, header_height=2, id="cited-by-table")
+
+
+    def on_mount(self) -> None:
+        """Generate table."""
+        title_width = 95
+
+        citations_table: DataTable[Text] = self.app.query_one("#cited-by-table", DataTable)
+        citations_table.cursor_type = "row"
 
         # Define Columns
-        citations_table.add_column('', justify='center', vertical='middle')
-        citations_table.add_column('Title', ratio=3)
-        citations_table.add_column('Type', ratio=2)
-        citations_table.add_column('DOI')
-        citations_table.add_column('Cite Type', justify='center')
+        citations_table.add_column('Title', width=title_width)
+        citations_table.add_column('Type', width=20)
+        citations_table.add_column('DOI', width=5)
+        citations_table.add_column('Cite Type', width=12)
         citations_table.add_column('Publication Date')
         citations_table.add_column('Cited by count')
 
         # Yield Rows
-        for idx, cited_by_work in enumerate(self.citations_list):
+        for idx, cited_by_work in enumerate(self.citations_list, start=1):
             work = cited_by_work.work
 
             work_pdf_url = work.primary_location.pdf_url if work.primary_location else None
@@ -62,19 +73,19 @@ class CitedByTable(Static):
             citation_type = f"[#909d63]{ct_value.name}[/]" if ct_value is CitationType.TypeA else f"[#bc5653]{ct_value.name}[/]"
 
             doi = work.ids.doi
-            doi_url = f"""[@click=app.open_link('{quote(str(doi))}')]DOI[/]""" if doi else "-"
+            doi_url = f"""[u][@click=app.open_link('{quote(str(doi))}')]DOI[/][/u]""" if doi else "-"
 
+            row_height = math.ceil(len(work.title) / title_width) + 1
             citations_table.add_row(
-                str(idx),
                 Text.from_markup(title, overflow='ellipsis'),
-                work.type,
+                Text(work.type),
                 Text.from_markup(doi_url, overflow='ellipsis'),
-                citation_type,
-                work.publication_date,
-                str(work.cited_by_count)
+                Text.from_markup(citation_type),
+                Text(work.publication_date),
+                Text(str(work.cited_by_count)),
+                height=row_height,
+                label=Text.from_markup(f"{idx}", justify="center")
             )
-
-        yield Static(citations_table, classes='citations-table')
 
 
 class WorkModal(Modal[None]):
@@ -127,9 +138,8 @@ class WorksTable(Static):
     """Table with all works produced by an author."""
 
     DEFAULT_CSS = """
-    WorksTable {
+    WorksTable DataTable {
         height: auto;
-        margin: 1 0 0 0;
     }
     """
 
@@ -152,15 +162,15 @@ class WorksTable(Static):
         last_pub_year = self.report.works[-1].work.publication_year
 
         yield Label(f"Works from {first_pub_year} to {last_pub_year}", classes="table-title")
-        yield DataTable(zebra_stripes=True)
+        yield DataTable(zebra_stripes=True, header_height=2)
 
 
     def on_mount(self) -> None:
         """Generate Table."""
         title_width = 100
 
-        work_table: DataTable[Text] = self.app.query_one(DataTable)
-        work_table.cursor_type = "cell"
+        work_table: DataTable[Text] = self.query_one(DataTable)
+        work_table.cursor_type = "row"
 
         work_table.add_column('Title', width=title_width)
         work_table.add_column('Type', width=20)
@@ -171,7 +181,7 @@ class WorksTable(Static):
         for idx, work_report in enumerate(self.report.works, start=1):
             work = work_report.work
             doi = work.ids.doi
-            doi_url = f"""[u][@click=app.open_link("{quote(str(doi))}")]DOI[/][u/]""" if doi else "-"
+            doi_url = f"""[u][@click=app.open_link("{quote(str(doi))}")]DOI[/][/u]""" if doi else "-"
 
             row_height = math.ceil(len(work.title) / title_width) + 1
             work_table.add_row(
@@ -181,7 +191,7 @@ class WorksTable(Static):
                 Text(work.publication_date),
                 Text(str(work.cited_by_count)),
                 height=row_height,
-                label=Text.from_markup(f"[u]{idx}[/]")
+                label=Text.from_markup(f"""[u]{idx}[/u]""", justify="center")
             )
 
 
