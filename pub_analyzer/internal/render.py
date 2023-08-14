e7c52fd35714a920fa5cb6893d2f253b5043cf29
Alejandro Gaspar <miguel.gaspar03@hotmail.com>
2023-08-13 22:22:42 -0600

    Split render report in two steps

--- pub_analyzer/internal/render.py
+++ pub_analyzer/internal/render.py
@@ -8,16 +8,21 @@ from jinja2 import Environment, FileSystemLoader
 from pub_analyzer.models.report import AuthorReport, InstitutionReport
 
 
-async def render_report(report: AuthorReport | InstitutionReport, file_path: pathlib.Path) -> bytes:
+async def render_template_report(report: AuthorReport | InstitutionReport) -> str:
     """Render report pdf."""
-    if type(report) is AuthorReport:
+    if isinstance(report, AuthorReport):
         templates_path = pathlib.Path(__file__).parent.resolve().joinpath("templates/author")
-    if type(report) is InstitutionReport:
+    if isinstance(report, InstitutionReport):
         raise NotImplementedError
 
     # Render template
     env = Environment(loader=FileSystemLoader(searchpath=templates_path), enable_async=True, trim_blocks=True, lstrip_blocks=True)
-    template_render = await env.get_template("report.typ").render_async(report=report)
+    return await env.get_template("report.typ").render_async(report=report)
+
+
+async def render_report(report: AuthorReport | InstitutionReport, file_path: pathlib.Path) -> bytes:
+    """Render report pdf."""
+    template_render = await render_template_report(report=report)
 
     # Write template to typst file
     root = file_path.parent
@@ -29,7 +34,7 @@ async def render_report(report: AuthorReport | InstitutionReport, file_path: pat
     compiler = typst.Compiler(root)
     pdf_render = compiler.compile(temp_file.name)
 
-    if type(pdf_render) is bytes:
+    if isinstance(pdf_render, bytes):
         return pdf_render
     else:
         raise SyntaxError
