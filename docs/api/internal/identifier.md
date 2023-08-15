44c0969f1a25edb68e3b67897fbeec0cc4e96ba2
Alejandro Gaspar <miguel.gaspar03@hotmail.com>
2023-08-15 00:33:54 -0600

    Add identifier module documentation

---
+++ docs/api/internal/identifier.md
@@ -0,0 +1,15 @@
+# Identifier
+
+Within OpenAlex, IDs consist of two core components: a consistent base, `https://openalex.org/`, and a unique resource identifier called the Key, such as `A000000000`. Consequently, a complete ID is structured as `https://openalex.org/A000000000`.
+
+While querying the API using the full ID is an option, at **pub analyzer**, we prefer utilizing only the key. This choice enhances the clarity of logs and proves especially helpful during debugging.
+
+Hence, the subsequent functions have been devised. They accept entity models as input and yield the corresponding keys from their IDs.
+
+!!! info
+    For further details about the OpenAlex IDs, consult the [documentation](https://docs.openalex.org/how-to-use-the-api/get-single-entities#the-openalex-id){target=_blank}.
+
+!!! tip "How to recognize the type of entity?"
+    You can deduce the entity's type from the ID itself. This is possible because all keys commence with a letter that corresponds to the entity's type: **W**(ork), **A**(uthor), **S**(ource), **I**(nstitution), **C**(oncept), **P**(ublisher), or **F**(under). Quite convenient, isn't it?
+
+::: pub_analyzer.internal.identifier
--- pub_analyzer/internal/identifier.py
+++ pub_analyzer/internal/identifier.py
@@ -1,4 +1,4 @@
-"""Extract Ids functions."""
+"""Functions to extract OpenAlex IDs from Models."""
 
 from pub_analyzer.models.author import Author, AuthorResult, DehydratedAuthor
 from pub_analyzer.models.institution import DehydratedInstitution, Institution, InstitutionResult
@@ -6,7 +6,24 @@ from pub_analyzer.models.work import Work
 
 
 def get_author_id(author: Author | AuthorResult | DehydratedAuthor) -> str:
-    """Extract OpenAlex ID from Author Model."""
+    """Extract OpenAlex ID from author Model.
+
+    Args:
+        author: Author model instance.
+
+    Returns:
+        Author OpenAlex ID.
+
+    Example:
+        ```python
+        from pub_analyzer.internal.identifier import get_author_id
+        from pub_analyzer.models.author import DehydratedAuthor
+
+        author = DehydratedAuthor(id="https://openalex.org/A000000000")
+        print(get_author_id(author))
+        # 'A000000000'
+        ```
+    """
     if author.id.path:
         return author.id.path.rpartition('/')[2]
     else:
@@ -14,7 +31,24 @@ def get_author_id(author: Author | AuthorResult | DehydratedAuthor) -> str:
 
 
 def get_institution_id(institution: Institution | InstitutionResult | DehydratedInstitution) -> str:
-    """Extract OpenAlex ID from Author Model."""
+    """Extract OpenAlex ID from institution Model.
+
+    Args:
+        institution: Institution model instance.
+
+    Returns:
+        Institution OpenAlex ID.
+
+    Example:
+        ```python
+        from pub_analyzer.internal.identifier import get_institution_id
+        from pub_analyzer.models.institution import DehydratedInstitution
+
+        institution = DehydratedInstitution(id="https://openalex.org/I000000000", **kargs)
+        print(get_institution_id(institution))
+        # 'I000000000'
+        ```
+    """
     if institution.id.path:
         return institution.id.path.rpartition('/')[2]
     else:
@@ -22,7 +56,24 @@ def get_institution_id(institution: Institution | InstitutionResult | Dehydrated
 
 
 def get_work_id(work: Work) -> str:
-    """Extract OpenAlex ID from Work Model."""
+    """Extract OpenAlex ID from Work Model.
+
+    Args:
+        work: Work model instance.
+
+    Returns:
+        Work OpenAlex ID.
+
+    Example:
+        ```python
+        from pub_analyzer.internal.identifier import get_work_id
+        from pub_analyzer.models.work import Work
+
+        work = Work(id="https://openalex.org/W000000000", **kargs)
+        print(get_work_id(work))
+        # 'W000000000'
+        ```
+    """
     if work.id.path:
         return work.id.path.rpartition('/')[2]
     else:
