2a59a04ccbfd0f7b1af009f5ed2e762c524540aa
Alejandro Gaspar <miguel.gaspar03@hotmail.com>
2025-02-13 11:52:11 -0600

    Update dev dependencies

--- pub_analyzer/widgets/search/__init__.py
+++ pub_analyzer/widgets/search/__init__.py
@@ -4,8 +4,8 @@ from .core import FinderWidget, SearchBar
 from .results import AuthorResultWidget, InstitutionResultWidget
 
 __all__ = [
-    "FinderWidget",
-    "SearchBar",
     "AuthorResultWidget",
+    "FinderWidget",
     "InstitutionResultWidget",
+    "SearchBar",
 ]
